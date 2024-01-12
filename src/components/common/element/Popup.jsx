4d7387463302ddce04dc53899978cf5551b60357
TH <je64768035@gmail.com>
2024-01-12 11:22:15 +0900

    popup inner closed 버튼

--- src/components/common/element/Popup.jsx
+++ src/components/common/element/Popup.jsx
@@ -1,5 +1,7 @@
 import { Children, cloneElement, isValidElement } from "react";
 import styled from "styled-components";
+import { Button } from "assets/styles/StyledCm";
+import { colors, transitions } from "assets/styles/Variable";
 
 function Popup ({ children, popupOn, ...props }){
   const popupClosed = () => { // 닫기
@@ -12,21 +14,82 @@ function Popup ({ children, popupOn, ...props }){
     return child
   })
   return (
-    <PopupWrap>
-      {childrenProps}
+    <PopupLayer>
+      <PopupInner className="popup__inner">
+        {childrenProps}
+        <CloseBtn onClick={()=> popupClosed()}>
+          <span>닫기</span>
+        </CloseBtn>
+      </PopupInner>
       <Dimmed onClick={()=>popupClosed()}/>
-    </PopupWrap>
+
+      
+
+    </PopupLayer>
   )
 }
 export default Popup;
 
-const PopupWrap = styled.div`
+const PopupLayer = styled.div`
   position:fixed;
   z-index:10;
   top:0;
   left:0;
   width:100%;
   height:100%;
+`;
+const PopupInner = styled.div`
+  position:absolute;
+  z-index:10;
+  top:50%;
+  left:50%;
+  width:80%;
+  min-width:500px;
+  height:80%;
+  min-height:500px;
+  padding:30px;
+  background:#fff;
+  border:1px solid red;
+  transform: translate(-50%, -50%);
+`;
+
+const CloseBtn = styled(Button)`
+  position:absolute;
+  top:30px;
+  right:30px;
+  width:30px;
+  height:30px;
+  &:hover, &:focus { 
+    &>span{
+      transform: rotate(-180deg);
+      &::before,&::after{
+        background:${colors.blue};
+      }
+    }
+  }
+  &>span{
+    display:block;
+    position:relative;
+    width:100%;
+    height:100%;
+    text-indent:-9999px;
+    transition:${transitions.base};
+    &::before, &::after{
+      position:absolute;
+      top:50%;
+      left:0;
+      width:100%;
+      height:2px;
+      border-radius:50%;
+      background:${colors.bgBlack};
+      transition:${transitions.base};
+      transform: translateY(-50%) rotate(45deg);
+      content:'';
+    }
+    &::after{
+      transform: translateY(-50%) rotate(-45deg) ;
+    }
+  }
 `;
 
 const Dimmed = styled.div`
@@ -35,5 +98,5 @@ const Dimmed = styled.div`
   left:0;
   width:100%;
   height:100%;
-  background: rgba(0,0,0,.5);
+  background: rgba(0,0,0,.7);
 `;
